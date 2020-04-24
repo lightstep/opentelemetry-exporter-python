99a98a8aa83612479b8ea427e39fbfeedc777f5c
Alex Boten <aboten@lightstep.com>
2020-04-24 09:27:51 -0700

    updating example and changelog

--- examples/tracer.py
+++ examples/tracer.py
@@ -2,20 +2,30 @@
 import os
 
 from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerSource
-from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
 from opentelemetry.ext.lightstep import LightStepSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
 
-trace.set_preferred_tracer_source_implementation(lambda T: TracerSource())
+
 # configure the LightStepSpanExporter as our exporter
 exporter = LightStepSpanExporter(
-    name="test-service", token=os.getenv("LIGHTSTEP_TOKEN", ""), verbosity=5
+    name=os.getenv("LIGHTSTEP_SERVICE_NAME", "test-service-name"),
+    token=os.getenv("LIGHTSTEP_ACCESS_TOKEN", ""),
+    verbosity=5,
+    host="ingest.staging.lightstep.com",
+    service_version=os.getenv("LIGHTSTEP_SERVICE_VERSION", "0.0.1"),
 )
 span_processor = BatchExportSpanProcessor(exporter)
-trace.tracer_source().add_span_processor(span_processor)
+
+
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(span_processor)
 
 tracer = trace.get_tracer("lightstep-exporter-example")
-with tracer.start_as_current_span("foo"):
+with tracer.start_as_current_span("foo") as span:
+    span.set_attribute("platform", "osx")
+    span.set_attribute("version", "1.2.3")
+    span.add_event("event in foo", {"name": "foo1"})
     with tracer.start_as_current_span("bar"):
         with tracer.start_as_current_span("baz") as s:
             s.set_attribute("test", "bah")
